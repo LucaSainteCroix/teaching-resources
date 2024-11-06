e4b2e8e9262d53938bd4bd815220495d6c9ea68a
luca saintecroix <luca.saintecroix@gmail.com>
2024-11-06 08:56:41 +0100

    added 03-orchestration

---
+++ Ressources/MLOps/mlops-zoomcamp/03-orchestration/pipeline_example.py
@@ -0,0 +1,18 @@
+def download_data(year, month):
+    ...
+    return df
+
+def prepare_data(df):
+    ...
+    return df
+
+def feature_engineering(df):
+    ...
+    return X, y
+
+def find_best_model(X, y):
+    ...
+    return params
+
+def train_model(X, y, params):
+                
