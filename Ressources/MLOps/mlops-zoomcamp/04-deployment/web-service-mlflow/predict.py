70b29166432417f7e4a0252d583c5237554caa0a
luca saintecroix <luca.saintecroix@gmail.com>
2024-12-18 17:16:12 +0100

    suite correction 04-deployment

--- Ressources/MLOps/mlops-zoomcamp/04-deployment/web-service-mlflow/predict.py
+++ Ressources/MLOps/mlops-zoomcamp/04-deployment/web-service-mlflow/predict.py
@@ -1,15 +1,38 @@
 import os
 import pickle
 
-import mlflow
 from flask import Flask, request, jsonify
 
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
 
-RUN_ID = os.getenv('RUN_ID')
+RUN_ID = os.getenv('MLFLOW_BEST_RUN_ID')
 
-logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
+# Import the client object from the SDK library
+from azure.storage.blob import BlobClient
+
+# Retrieve the connection string from an environment variable. Note that a
+# connection string grants all permissions to the caller, making it less
+# secure than obtaining a BlobClient object using credentials.
+conn_string = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
+
+# Create the client object for the resource identified by the connection
+# string, indicating also the blob container and the name of the specific
+# blob we want.
+blob_client = BlobClient.from_connection_string(
+    conn_string,
+    container_name="lsc-mlflow",
+    blob_name=f"mlartifacts/1/{RUN_ID}/artifacts/model/model.pkl",
+)
+
+blob = blob_client.download_blob().readall()
+
+model = pickle.loads(blob)
+
+# logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
 # logged_model = f'runs:/{RUN_ID}/model'
-model = mlflow.pyfunc.load_model(logged_model)
+# model = mlflow.pyfunc.load_model(blob)
+
 
 
 def prepare_features(ride):
